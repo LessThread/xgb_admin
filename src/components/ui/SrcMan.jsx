e35e92a6193b21e76023283f317ef6bf16849d06
lyc_tju <allforone@tju.edu.cn>
2022-06-11 23:58:06 +0800

    fixcss

--- src/components/ui/SrcMan.jsx
+++ src/components/ui/SrcMan.jsx
@@ -11,6 +11,10 @@ import { fetchApi } from '../../callApi';
 import { ADMIN_nav_list, getNavAllArticle } from '../../constants/api/navi';
 import { removeArticle, deleteArticle } from '../../constants/api/source';
 import { getCateLists } from '../../constants/api/category';
+import postcssFlexbugsFixes from 'postcss-flexbugs-fixes';
+
+import './banners/Src.scss';
+
 const { TabPane } = Tabs;
 const { Option, OptGroup } = Select;
 const success = (content) => {
@@ -34,104 +38,130 @@ class Src extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            nav_id: 0,
+            nav_id: 87,
             show: null,
             pageNum: 1,
             pageSize: 10,
-            accdat: null,
+            accdat: [],
+            isFinished: 0,
         };
         this.handleClick = this.handleClick.bind(this);
     }
 
-    componentDidMount() { //获取sideMenu和第一个数组的信息。
-        fetch(``)
+    componentWillMount() { //获取sideMenu和第一个数组的信息。
+        fetch(`http://120.48.17.78:8080/api/Article/getByPage?nav_id=` + this.state.nav_id + `&pageNum=` + this.state.pageNum + `&pageSize=` + this.state.pageSize, setting)
+            .then(function (response) {
+                return response.json();
+            })
+            .then((res) => {
+                console.log("@")
+                console.log(res)
+                this.setState({
+                    accdat: res.data.res,
+                    isFinished: 1,
+                },
+                    () => {
+                        console.log(this.state.accdat)
+                        console.log(this.state.accdat[0])
+
+                    })
+            })
     }
 
+    handleClick = (id, num) => {
 
-    show() {
-        if (this.state.nav_id !== 0) {
+        num = 1;
+        fetch(`http://120.48.17.78:8080/api/Article/getByPage?nav_id=` + id + `&pageNum=` + num + `&pageSize=` + this.state.pageSize, setting)
+            .then(function (response) {
+                return response.json();
+            })
+            .then((res) => {
+                console.log("@")
+                console.log(res)
+                this.setState({
+                    accdat: res.data.res,
+                    isFinished: 1,
+                },
+                    () => {
+                        console.log(this.state.accdat)
+                        console.log(this.state.accdat[0])
+
+                    })
+            })
+
+    }
+
+    show = () => {
+
+        console.log("this is show")
+        console.log(this.state.accdat)
+
+        if (this.state.isFinished) {
             return this.state.accdat.map((item, index) => {
                 return (
-                    <div key={index} style={{}}>
-                        {item.title}
-                        <div>{this.state.pageNum}</div>
+                    <div style={{ display: `flex`, }}>
+                        <input type="checkbox" className='checkbox' />
+                        <p className='inP'>{index}</p>
+                        <h3>{item.title}</h3>
                     </div>
                 )
             })
         }
-        else {
-            return (
-                <div>
-                    waiting
-                </div>
-            )
-        }
     }
 
-
-    handleClick($id) {
-        this.state.nav_id = $id;
-        this.setState({
-            nav_id: $id
-        })
-
-        console.log(this.state.id);
-
-        if ($id !== 0) {
-            fetch(`http://120.48.17.78:8080/api/Article/getByPage?nav_id=` + this.state.nav_id + `&pageNum=` + this.state.pageNum + `&pageSize=` + this.state.pageSize, setting)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then((rest) => {
-                    console.log("@")
-                    console.log(rest)
-                    this.setState({
-                        accdat: rest.data.res
-                    },
-                        () => {
-                            console.log(this.state.accdat)
-                            // this.show()
-                        })
-                })
-        }
-    }
 
 
     render() {
 
         return (
-            <div>
+            <div >
                 <BreadcrumbCustom first="资源管理" />
 
-                <div>
+                <div id="root">
 
-                    <div id="sad">
-                        <button onClick={() => this.handleClick(86)}>交流园地</button>
+                    <div id="left">
+                        <div>
+                            <div id="sad">
+                                <button onClick={() => this.handleClick(87)}>交流园地</button>
+                            </div>
+                            <div>
+                                <button onClick={() => this.handleClick(86)}>理论学习</button>
+                            </div>
+                            <div>
+                                <button onClick={() => this.handleClick(89)}>学工资讯</button>
+                            </div>
+                            <div>
+                                <button onClick={() => this.handleClick(83)}>专题快报</button>
+                            </div>
+                            <div>
+                                <button onClick={() => this.handleClick(84)}>一院一品</button>
+                            </div>
+                            <div>
+                                <button onClick={() => this.handleClick(85)}>通知公告</button>
+                            </div>
+                        </div>
+
+                        <div id="operate">
+                            <div>
+                                <button>移动</button>
+                                <button>删除</button>
+                            </div>
+
+                            <div>
+                                <h3>移动到</h3>
+                            </div>
+                        </div>
                     </div>
+
+                </div>
+
+
+                <div id="showinf2">
                     <div>
-                        <div>理论学习</div>
-                    </div>
-                    <div>
-                        <div>学工资讯</div>
-                    </div>
-                    <div>
-                        <div>专题快报</div>
-                    </div>
-                    <div>
-                        <div>一院一品</div>
-                    </div>
-                    <div>
-                        <div>通知公告</div>
+                        {this.show()}
                     </div>
                 </div>
 
-                TEST
-                <div id="showinf">
-                    id:{this.state.nav_id}
-                </div>
-                <div>
-                    {this.show()}
-                </div>
 
             </div >
         )
